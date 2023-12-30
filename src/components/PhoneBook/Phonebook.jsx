cf079dd60ad8d55c699e6fdc4237b7b2eea74c8b
Volodya Mikhnov <vvvmikhnov@gmail.com>
2023-12-31 00:35:33 +0200

    changed

--- src/components/PhoneBook/Phonebook.jsx
+++ src/components/PhoneBook/Phonebook.jsx
@@ -1,20 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import styles from "./Phonebook.module.css";
 import ContactForm from "./Refactor/ContactForm";
 import Filter from "./Refactor/Filter";
 import ContactList from "./Refactor/ContactList";
-import { nanoid } from "nanoid";
-import { saveContact, deleteContact, updateFilter, clearContacts } from "../../Redux/contactSlice"; 
+import { saveContact } from "../../Redux/contactSlice";
 
 const Phonebook = () => {
   const dispatch = useDispatch();
   const contacts = useSelector((state) => state.contacts.contacts);
-  const filter = useSelector((state) => state.contacts.filter);
-
-  const [name, setName] = useState("");
-  const [number, setNumber] = useState("");
-  const [showDeleted] = useState(false);
 
   useEffect(() => {
     const storedContacts = JSON.parse(localStorage.getItem("contacts"));
@@ -22,64 +16,20 @@ const Phonebook = () => {
       dispatch(saveContact(storedContacts));
     }
   }, [dispatch]);
-  
 
   useEffect(() => {
     localStorage.clear();
     localStorage.setItem("contacts", JSON.stringify(contacts));
   }, [contacts]);
 
-  const change = (e) => {
-    const { name, value } = e.target;
-    if (name === "name") {
-      setName(value);
-    } else if (name === "number") {
-      setNumber(value);
-    }
-  };
-
-  const submit = (e) => {
-    e.preventDefault();
-    if (contacts.some(contact => contact.name.toLowerCase() === name.toLowerCase())) {
-      alert(`${name} is already in contacts`);
-      return;
-    }
-    const newContact = {
-      id: nanoid(),
-      name: name,
-      number: number,
-    };
-
-    dispatch(saveContact(newContact));
-    setName("");
-    setNumber("");
-  };
-
-  const handleDeleteContact = (id) => {
-    dispatch(deleteContact(id));
-  };
-
-  const handleFilterChange = (value) => {
-    dispatch(updateFilter(value));
-  };
-
-  const handleClearContacts = () => {
-    dispatch(clearContacts());
-  };
-  const filteredContacts = showDeleted
-    ? contacts
-    : contacts.filter(
-        (contact) => contact.name && contact.name.toLowerCase().includes(filter.toLowerCase())
-      );
-
   return (
     <div className={styles.container}>
       <h1>Phonebook</h1>
-      <ContactForm name={name} number={number} onChange={change} onSubmit={submit} />
+      <ContactForm />
 
       <h2>Contacts</h2>
-      <Filter value={filter} onChange={handleFilterChange} />
-      <ContactList contacts={filteredContacts} deleteContact={handleDeleteContact} clearContacts={handleClearContacts}/>
+      <Filter />
+      <ContactList />
     </div>
   );
 };
--- src/components/PhoneBook/Refactor/ContactForm.jsx
+++ src/components/PhoneBook/Refactor/ContactForm.jsx
@@ -1,21 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { saveContact } from "../../../Redux/contactSlice";
+import { nanoid } from "nanoid";
 
-const ContactForm = ({ name, number, onChange, onSubmit }) => {
+const ContactForm = () => {
+  const dispatch = useDispatch();
+  const [name, setName] = useState("");
+  const [number, setNumber] = useState("");
+  const contacts = useSelector((state) => state.contacts.contacts);
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    if (name === "name") {
+      setName(value);
+    } else if (name === "number") {
+      setNumber(value);
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (contacts.some(contact => contact.name.toLowerCase() === name.toLowerCase())) {
+      alert(`${name} is already in contacts`);
+      return;
+    }
+    const newContact = {
+      id: nanoid(), 
+      name,
+      number,
+    };
+    dispatch(saveContact(newContact));
+    setName("");
+    setNumber("");
+  };
+
   return (
-    <form onSubmit={onSubmit}>
+    <form onSubmit={handleSubmit}>
       <label>
         Name
-        <input type="text" name="name" value={name} onChange={onChange} required />
+        <input type="text" name="name" value={name} onChange={handleInputChange} required />
       </label>
       <label>
         Number
-        <input type="tel" name="number" value={number} onChange={onChange} required />
+        <input type="tel" name="number" value={number} onChange={handleInputChange} required />
       </label>
       <button type="submit">
         Add Contact
       </button>
     </form>
   );
-}
+};
 
 export default ContactForm;
